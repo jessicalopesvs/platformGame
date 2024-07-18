6c5082c73085af6548a90ab9eb066bdc4c9a9713
jessicalopesvs <37242604+jessicalopesvs@users.noreply.github.com>
2024-07-18 15:59:06 +0100

    collision working

--- code_start/player.py
+++ code_start/player.py
@@ -2,15 +2,24 @@ from settings import *
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.Surface((48, 56))
         self.image.fill('red')
+
+
+        # rects
         self.rect = self.image.get_frect(topleft=pos)
+        self.old_rect = self.rect.copy()
 
         # movement
         self.direction = vector(0, 0)
         self.speed = 200
+        self.gravity = 1300
+
+        # collision
+
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -19,11 +28,41 @@ class Player(pygame.sprite.Sprite):
             input_vector.x += 1
         if keys[pygame.K_a]:
             input_vector.x -= 1
-        self.direction = input_vector.normalize() if input_vector else input_vector
+        self.direction.x = input_vector.normalize().x if input_vector.x else input_vector.x
 
     def move(self, dt):
-        self.rect.topleft += self.direction * self.speed * dt
+        #horizontal
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+
+        #vertical
+        self.direction.y += self.gravity / 2 * dt
+        self.rect.y += self.direction.y * dt
+        self.direction.y += self.gravity / 2 * dt
+        self.collision('vertical')
+
+    def collision(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if axis == 'horizontal':
+                    # Left collision
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+                    # Right collision
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.rect.left:
+                        self.rect.right = sprite.rect.left
+                elif axis == 'vertical':
+                    # Floor collision
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.rect.top:
+                        self.rect.bottom = sprite.rect.top
+                        self.direction.y = 0  # Stop falling
+                    # Ceiling collision
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+                        self.direction.y = 0  # Stop rising
+
 
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.input()
         self.move(dt)
