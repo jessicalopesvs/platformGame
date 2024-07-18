172afb50a41ae55d9f9e9caaa4f9a3c6ef435b57
jessicalopesvs <37242604+jessicalopesvs@users.noreply.github.com>
2024-07-18 14:10:38 +0100

    player vector working

--- code_start/level.py
+++ code_start/level.py
@@ -1,5 +1,7 @@
+from code_start.player import Player
 from settings import *
 from sprite import Sprite
+from player import Player
 
 class Level:
     def __init__(self, tmx_map):
@@ -7,8 +9,8 @@ class Level:
 
         #groups
         self.all_sprites = pygame.sprite.Group()
-
         self.setup(tmx_map)
+
 
     def setup(self, tmx_map):
         for x,y,surf in tmx_map.get_layer_by_name('Terrain').tiles():
@@ -16,8 +18,9 @@ class Level:
 
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
-                print(obj)
+                Player((obj.x, obj.y), self.all_sprites)
 
-    def run(self):
+    def run(self, dt):
+        self.all_sprites.update(dt)
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
--- code_start/main.py
+++ code_start/main.py
@@ -9,18 +9,20 @@ class Game:
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Get a job, Jéssica')
+        self.clock = pygame.time.Clock()
 
         self.tmx_maps = {0: load_pygame(join('.', 'data', 'levels', 'omni.tmx'))}
         self.current_stage = Level(self.tmx_maps[0])
 
     def run(self):
         while True:
+            dt = self.clock.tick() / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-            self.current_stage.run()
+            self.current_stage.run(dt)
             pygame.display.update()
 
 
